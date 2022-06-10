ed0371a328f18551f075ea98b1576ffb1578ef70
jeffersd <dillon_jeffers@fastmail.com>
2022-06-09 22:24:31 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const axios = require("axios"),
+    parse5 = require("parse5"),
+    url = "https://store.x-plane.org/Rotate-MD-11_p_1580.html";
+
+function getHtml (url) {
+    return "";
+}
+
+function findPriceObject (html) {
+    return {};
+}
+
+function isOnSale (price) {
+    return false;
+}
+
+function checkIfAddonIsOnSale (url) {
+    return isOnSale(findPriceOjbect(getHtml(url)));
+}
+
+
+console.log(`addon is on sale?: ${checkIfAddonIsOnSale(url)}`);
