d77d51e6e845de2cc5bd446c918b3d607b938c8c
jeffersd <dillon_jeffers@fastmail.com>
2022-06-11 05:57:37 -0400

    add additional optimizations to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-closure-compiler ./index.js > ./out.js
+closure-compiler --assume_function_wrapper ./index.js > ./out.js
 mv /Users/dillonjeffers/workspace/check-md-11/out.js /Users/dillonjeffers/scripts/check-md-11/
