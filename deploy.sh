d22b3489144619a295649c1faa07d7edd0ffba0d
jeffersd <dillon_jeffers@fastmail.com>
2022-12-04 02:51:10 -0500

    add google-closure-compiler as a dev dependency
    This lets us keep track of the version more easily and lets us specify
    the tool for the project. Otherwise it is not as explicit that the project
    depends on that tool, nor is it as clear if it becomes outdated.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-closure-compiler --assume_function_wrapper ./index.js > ./out.js
+google-closure-compiler --assume_function_wrapper ./index.js > ./out.js
 mv /Users/dillonjeffers/workspace/check-md-11/out.js /Users/dillonjeffers/scripts/check-md-11/
