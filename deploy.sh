c1c95dce57e38ef64b28a7fde5b938cbf499843e
jeffersd <dillon_jeffers@fastmail.com>
2022-12-05 07:21:39 -0500

    add more closure compiler flags
    This doesn't seem to change the script.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-google-closure-compiler --assume_function_wrapper ./index.js > ./out.js
+google-closure-compiler --compilation_level SIMPLE --strict_mode_input --assume_function_wrapper ./index.js > ./out.js
 mv /Users/dillonjeffers/workspace/check-md-11/out.js /Users/dillonjeffers/scripts/check-md-11/
