e40f0753006f4021132ad8e991b788536bf2ffa4
Dillon Jeffers <dillon_jeffers@fastmail.com>
2024-01-01 17:30:47 -0500

    update deploy script to use HOME environment variable

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 google-closure-compiler --compilation_level SIMPLE --strict_mode_input --assume_function_wrapper ./index.js > ./out.js
-mv /Users/dillonjeffers/workspace/check-md-11/out.js /Users/dillonjeffers/scripts/check-md-11/
+mv "${HOME}/workspace/check-md-11/out.js" "${HOME}/scripts/check-md-11/"
